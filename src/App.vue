1922bd253ac1cd3bbe59273b1d85037a7d16bcfe
balmha <nico_96_7@hotmail.com>
2018-08-31 15:59:26 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+    <Gallery/>
+  </div>
+</template>
+
+<script>
+import Gallery from "./components/Gallery";
+
+export default {
+  name: "App",
+  components: {
+    Gallery
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
